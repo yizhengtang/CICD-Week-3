102841e5c4defa358dcb7406319f3dba8e414f53
Yi Zheng Tang <g00425075@atu.ie>
2024-10-14 23:19:46 +0100

    Created a new Product class using lombok.

---
+++ src/main/java/ie/atu/week3/Product.java
@@ -0,0 +1,15 @@
+package ie.atu.week3;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private String id;
+    private String name;
+    private String category;
+    private double price;
+}
